fe6a508486fc2335889199f8667b8c33333dcc7e
popshia <B123292179@gmail.com>
2022-01-03 14:48:39 +0800

    add gotop toggle.

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -159,7 +159,7 @@ local mappings = {
 	t = {
 		name = "Terminal",
 		n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
-		t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
+		g = { "<cmd>lua _GOTOP_TOGGLE()<cr>", "GoTop" },
 		p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
 		f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
 		h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
