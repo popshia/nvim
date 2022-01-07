b6d69bcc49b6d9c802e27b3f4320e0060cfb71a4
popshia <popshia@cycu.org.tw>
2022-01-07 10:56:49 +0800

    modified font.

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -34,7 +34,7 @@ local options = {
 	wrap = false, -- display lines as one long line
 	scrolloff = 8, -- is one of my fav
 	sidescrolloff = 8, -- side scrolloff with 8 char
-	guifont = "JetBrainsMonoMedium Nerd Font Mono:h18", -- the font used in graphical neovim applications
+	guifont = "JetBrainsMono Nerd Font Mono:h18", -- the font used in graphical neovim applications
 }
 
 vim.opt.shortmess:append("c")
