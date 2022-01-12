524582fc4004b992c8b7247c32de4aff373e52a2
pophsia <b123292179@gmail.com>
2022-01-12 14:28:40 +0800

    add window navigation shortcuts.

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -21,8 +21,8 @@ vim.g.maplocalleader = " "
 -- Normal --
 -- Better window navigation
 map("n", "<C-h>", "<C-w>h", opts)
--- map("n", "<C-j>", "<C-w>j", opts)
--- map("n", "<C-k>", "<C-w>k", opts)
+map("n", "<C-j>", "<C-w>j", opts)
+map("n", "<C-k>", "<C-w>k", opts)
 map("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize with arrows
