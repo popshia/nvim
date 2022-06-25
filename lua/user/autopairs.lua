24c39d2444093d8d2a6a958148c31c533ac2cb96
popshia <popshia@icloud.com>
2022-06-26 00:51:40 +0800

    add markdown concealing.

--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -4,15 +4,15 @@ if not status_ok then
 	return
 end
 
-npairs.setup {
+npairs.setup({
 	check_ts = true, -- treesitter integration
 	disable_filetype = { "TelescopePrompt" },
-}
+})
 
-local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
 	return
 end
 
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done {})
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({}))
