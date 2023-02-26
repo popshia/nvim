6b95068e08a3a089292b9c5d51a987f120179a47
popshia <popshia@gmail.com>
2023-02-26 17:12:32 +0800

    fix mason-lsp.

--- lua/user/lsp/mason-lsp.lua
+++ lua/user/lsp/mason-lsp.lua
@@ -3,59 +3,35 @@ if not status_ok then
 	return
 end
 
-local opts = {}
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+	return
+end
 
-mason_lsp.setup_handlers({
-	-- The first entry (without a key) will be the default handler
-	-- and will be called for each installed server that doesn't have
-	-- a dedicated handler.
-	function(server_name) -- default handler (optional)
-		require("lspconfig")[server_name].setup(opts)
-	end,
-	-- Next, you can provide a dedicated handler for specific servers.
-	-- For example, a handler override for the `rust_analyzer`:
-	["pyright"] = function()
-		local pyright_opts = require("user.lsp.server-configs.pyright")
-		opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-	end,
-	["lua_ls"] = function()
+local servers = {
+	"pyright",
+	"lua_ls",
+}
+
+mason_lsp.setup({
+	ensure_installed = { "pyright", "lua_ls" }
+})
+
+for _, server in pairs(servers) do
+	local opts = {
+		on_attach = require("user.lsp.handlers").on_attach,
+		capabilities = require("user.lsp.handlers").capabilities,
+	}
+
+	if server == "lua_ls" then
 		local lua_ls_opts = require("user.lsp.server-configs.lua_ls")
 		opts = vim.tbl_deep_extend("force", lua_ls_opts, opts)
 	end
-})
 
--- mason_lsp.setup({
--- 	ensure_installed = { "pyright", "lua_ls", "clangd" },
--- })
---
--- local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
--- if not lspconfig_status_ok then
--- 	return
--- end
---
--- local servers = {
--- 	"pyright",
--- 	"lua_ls",
--- 	"clangd",
--- 	"texlab"
--- }
---
---
--- for _, server in pairs(servers) do
--- 	opts = {
--- 		on_attach = require("user.lsp.handlers").on_attach,
--- 		capabilities = require("user.lsp.handlers").capabilities,
--- 	}
---
--- 	if server == "lua_ls" then
--- 		local lua_ls_opts = require("user.lsp.server-configs.lua_ls")
--- 		opts = vim.tbl_deep_extend("force", lua_ls_opts, opts)
--- 	end
---
--- 	if server == "pyright" then
--- 		local pyright_opts = require("user.lsp.server-configs.pyright")
--- 		opts = vim.tbl_deep_extend("force", pyright_opts, opts)
--- 	end
---
--- 	lspconfig[server].setup(opts)
--- end
+	if server == "pyright" then
+		local pyright_opts = require("user.lsp.server-configs.pyright")
+		opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+	end
+
+	lspconfig[server].setup(opts)
+end
