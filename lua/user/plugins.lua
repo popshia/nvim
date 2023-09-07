41391b0a08db055ecb51b6137a331343f553e097
noah lin <Noah.Lin@auo.com>
2023-09-07 10:10:23 +0800

    format code.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -25,17 +25,17 @@ end
 -- Install your plugins here
 lazy.setup({
 	-- Utility
-	"nvim-lua/plenary.nvim", -- useful lua functions used by lots of plugins
-	"nvim-lua/popup.nvim", -- vim popup api in neovim
-	"kyazdani42/nvim-web-devicons", -- file icons
-	"akinsho/toggleterm.nvim", -- terminal integration
-	"ahmedkhalf/project.nvim", -- project file search
+	"nvim-lua/plenary.nvim",            -- useful lua functions used by lots of plugins
+	"nvim-lua/popup.nvim",              -- vim popup api in neovim
+	"kyazdani42/nvim-web-devicons",     -- file icons
+	"akinsho/toggleterm.nvim",          -- terminal integration
+	"ahmedkhalf/project.nvim",          -- project file search
 	"lukas-reineke/indent-blankline.nvim", -- show tab and indents
-	"famiu/bufdelete.nvim", -- better buffer manipulation
-	"karb94/neoscroll.nvim", -- smooth scroll
-	"rcarriga/nvim-notify", -- vim notify
-	"kevinhwang91/nvim-hlslens", -- search with count
-	"norcalli/nvim-colorizer.lua", -- colorizer
+	"famiu/bufdelete.nvim",             -- better buffer manipulation
+	"karb94/neoscroll.nvim",            -- smooth scroll
+	"rcarriga/nvim-notify",             -- vim notify
+	"kevinhwang91/nvim-hlslens",        -- search with count
+	"norcalli/nvim-colorizer.lua",      -- colorizer
 
 	-- File explorer
 	"kyazdani42/nvim-tree.lua", -- nvim-tree file explorer
@@ -43,23 +43,23 @@ lazy.setup({
 	-- Editing support
 	"nguyenvukhang/nvim-toggler", -- toggle texts
 	"sitiom/nvim-numbertoggle", -- toggle between relative and absolute numbers
-	"mizlan/iswap.nvim", -- swap nodes using treesitter
-	"kylechui/nvim-surround", -- surround character manipulation
-	"windwp/nvim-autopairs", -- autopairs
-	"numToStr/Comment.nvim", -- easily comment stuff
+	"mizlan/iswap.nvim",       -- swap nodes using treesitter
+	"kylechui/nvim-surround",  -- surround character manipulation
+	"windwp/nvim-autopairs",   -- autopairs
+	"numToStr/Comment.nvim",   -- easily comment stuff
 
 	-- Motion
 	"phaazon/hop.nvim", -- hop to any word you like
 
 	-- Interface
-	"folke/which-key.nvim", -- show available keys after hitting space
-	"goolord/alpha-nvim", -- startup page
+	"folke/which-key.nvim",   -- show available keys after hitting space
+	"goolord/alpha-nvim",     -- startup page
 	"nvim-lualine/lualine.nvim", -- status line
 	"akinsho/bufferline.nvim", -- buffer tab line
 	"folke/todo-comments.nvim", -- todo comments highlighting
 
 	-- Markdown
-	"godlygeek/tabular", -- text filtering
+	"godlygeek/tabular",   -- text filtering
 	"preservim/vim-markdown", -- markdown mode
 	{
 		"iamcco/markdown-preview.nvim",
@@ -76,30 +76,30 @@ lazy.setup({
 		"hrsh7th/nvim-cmp", -- the completion plugin
 		dependencies = {
 			-- cmp sources
-			"hrsh7th/cmp-buffer", -- buffer completions
-			"hrsh7th/cmp-path", -- path completions
-			"hrsh7th/cmp-cmdline", -- cmdline completions
-			"hrsh7th/cmp-nvim-lsp", -- lsp completions
-			"hrsh7th/cmp-nvim-lua", -- lua completions
-			"saadparwaiz1/cmp_luasnip", -- snippet completions
+			"hrsh7th/cmp-buffer",        -- buffer completions
+			"hrsh7th/cmp-path",          -- path completions
+			"hrsh7th/cmp-cmdline",       -- cmdline completions
+			"hrsh7th/cmp-nvim-lsp",      -- lsp completions
+			"hrsh7th/cmp-nvim-lua",      -- lua completions
+			"saadparwaiz1/cmp_luasnip",  -- snippet completions
 			"lukas-reineke/cmp-under-comparator", -- sort completions
 			-- {
 			-- 	"tzachar/cmp-tabnine",   -- tabnine completions
 			-- 	build = "./install.sh"
 			-- },
 			-- Snippets
-			"L3MON4D3/LuaSnip", --snippet engine
+			"L3MON4D3/LuaSnip",    --snippet engine
 			"rafamadriz/friendly-snippets", -- a bunch of snippets to use
 		},
 	},
 
 	-- LSP
-	"neovim/nvim-lspconfig", -- enable LSP
-	"ray-x/lsp_signature.nvim", -- for lsp signatures
-	"utilyre/barbecue.nvim", -- winbar components
-	"jose-elias-alvarez/null-ls.nvim", -- null-ls
+	"neovim/nvim-lspconfig",            -- enable LSP
+	"ray-x/lsp_signature.nvim",         -- for lsp signatures
+	"utilyre/barbecue.nvim",            -- winbar components
+	"jose-elias-alvarez/null-ls.nvim",  -- null-ls
 	{
-		"williamboman/mason.nvim", -- external tool management
+		"williamboman/mason.nvim",      -- external tool management
 		dependencies = {
 			"williamboman/mason-lspconfig.nvim", -- mason lspconfig bridging
 		},
@@ -109,7 +109,7 @@ lazy.setup({
 	"MattesGroeger/vim-bookmarks", -- bookmarks
 
 	-- Telescope
-	"nvim-telescope/telescope.nvim", -- highly extendable fuzzy finder over lists
+	"nvim-telescope/telescope.nvim",          -- highly extendable fuzzy finder over lists
 	"tom-anders/telescope-vim-bookmarks.nvim", -- bookmarks integration
 	{
 		"nvim-telescope/telescope-fzf-native.nvim", -- fzf syntax support for telescope
@@ -125,9 +125,9 @@ lazy.setup({
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter-context", -- show treesitter context
 			"JoosepAlviste/nvim-ts-context-commentstring", -- comment based on cursor location
-			"simrat39/symbols-outline.nvim", -- symbol outlines
-			"RRethy/vim-illuminate", -- automatically highlighting other uses of the current word under the cursor
-			"SmiteshP/nvim-navic", -- status line components that shows current code context
+			"simrat39/symbols-outline.nvim",      -- symbol outlines
+			"RRethy/vim-illuminate",              -- automatically highlighting other uses of the current word under the cursor
+			"SmiteshP/nvim-navic",                -- status line components that shows current code context
 		},
 	},
 
--- lua/user/todo-comments.lua
+++ lua/user/todo-comments.lua
@@ -6,7 +6,7 @@ end
 local icons = require("user.icons")
 
 todo_comments.setup({
-	signs = true, -- show icons in the signs column
+	signs = true,   -- show icons in the signs column
 	sign_priority = 8, -- sign priority
 	-- keywords recognized as todo comments
 	keywords = {
@@ -20,9 +20,9 @@ todo_comments.setup({
 		REVIEW = { icon = icons.ui.Pencil .. " ", color = "#b0b846" },
 	},
 	gui_style = {
-		fg = "BOLD", -- The gui style to use for the fg highlight group.
-		bg = "BOLD", -- The gui style to use for the bg highlight group.
-		wide = "BOLD", -- The gui style to use for the bg highlight group.
+		fg = "BOLD",    -- The gui style to use for the fg highlight group.
+		bg = "BOLD",    -- The gui style to use for the bg highlight group.
+		wide = "BOLD",  -- The gui style to use for the bg highlight group.
 	},
 	merge_keywords = true, -- when true, custom keywords will be merged with the defaults
 	-- highlighting of the line containing the todo comment
@@ -30,16 +30,16 @@ todo_comments.setup({
 	-- * keyword: highlights of the keyword
 	-- * after: highlights after the keyword (todo text)
 	highlight = {
-		multiline = false, -- enable multine todo comments
-		multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
-		multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
-		before = "fg", -- "fg" or "bg" or empty
-		keyword = "fg", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
-		after = "fg", -- "fg" or "bg" or empty
+		multiline = false,         -- enable multine todo comments
+		multiline_pattern = "^.",  -- lua pattern to match the next multiline from the start of the matched keyword
+		multiline_context = 10,    -- extra lines that will be re-evaluated when changing a line
+		before = "fg",             -- "fg" or "bg" or empty
+		keyword = "fg",            -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
+		after = "fg",              -- "fg" or "bg" or empty
 		pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
-		comments_only = true, -- uses treesitter to match keywords in comments only
-		max_line_len = 400, -- ignore lines longer than this
-		exclude = {}, -- list of file types to exclude highlighting
+		comments_only = true,      -- uses treesitter to match keywords in comments only
+		max_line_len = 400,        -- ignore lines longer than this
+		exclude = {},              -- list of file types to exclude highlighting
 	},
 	search = {
 		command = "rg",
