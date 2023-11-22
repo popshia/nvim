235d868ab2e4f371b43b99f536350991e45bebb1
Noah Lin <popshia@gmail.com>
2023-11-22 21:08:08 +0800

    tweak colorscheme.

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -6,12 +6,15 @@ local M = {
 
 function M.config()
 	vim.cmd([[
-	  let g:gruvbox_material_palette = 'mix'
-	  let g:gruvbox_material_statusline_style = 'mix'
-	  let g:gruvbox_material_enable_italic = 1
-	  let g:gruvbox_material_enable_bold = 1
-	  let g:gruvbox_material_better_performance = 1
+		let g:gruvbox_material_background = "medium"
+		let g:gruvbox_material_foreground = "mix"
+		let g:gruvbox_material_statusline_style = "mix"
+		let g:gruvbox_material_enable_italic = 1
+		let g:gruvbox_material_enable_bold = 1
+		let g:gruvbox_material_better_performance = 1
 	]])
+
+	vim.o.background = "dark"
 	vim.cmd.colorscheme("gruvbox-material")
 end
 
