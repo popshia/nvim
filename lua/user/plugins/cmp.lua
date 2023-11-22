68836d657758f633e8397d53433f63ea4050872e
popshia <popshia@gmail.com>
2023-11-22 21:45:05 +0800

    tweak cmp keybindings.

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -65,8 +65,8 @@ function M.config()
 		mapping = {
 			["<Up>"] = cmp.mapping.select_prev_item(),
 			["<Down>"] = cmp.mapping.select_next_item(),
-			["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-			["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+			["<C-p>"] = cmp.mapping(cmp.mapping.scroll_docs(-2), { "i", "c" }),
+			["<C-n>"] = cmp.mapping(cmp.mapping.scroll_docs(2), { "i", "c" }),
 			["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
 			["<C-e>"] = cmp.mapping({
 				i = cmp.mapping.abort(),
