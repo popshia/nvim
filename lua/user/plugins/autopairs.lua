012fea57264de02eaa496068ca6521ae806aafe4
Noah Lin <popshia@gmail.com>
2023-11-27 15:05:03 +0800

    update editorconfig and tweak autopairs.

--- lua/user/plugins/autopairs.lua
+++ lua/user/plugins/autopairs.lua
@@ -78,60 +78,6 @@ function M.config()
 			:with_move(cond.none())
 			:with_del(cond.none()),
 	})
-
-	-- expand multiple pairs on enter key
-	local get_closing_for_line = function(line)
-		local i = -1
-		local clo = ""
-
-		while true do
-			i, _ = string.find(line, "[%(%)%{%}%[%]]", i + 1)
-			if i == nil then
-				break
-			end
-			local ch = string.sub(line, i, i)
-			local st = string.sub(clo, 1, 1)
-
-			if ch == "{" then
-				clo = "}" .. clo
-			elseif ch == "}" then
-				if st ~= "}" then
-					return ""
-				end
-				clo = string.sub(clo, 2)
-			elseif ch == "(" then
-				clo = ")" .. clo
-			elseif ch == ")" then
-				if st ~= ")" then
-					return ""
-				end
-				clo = string.sub(clo, 2)
-			elseif ch == "[" then
-				clo = "]" .. clo
-			elseif ch == "]" then
-				if st ~= "]" then
-					return ""
-				end
-				clo = string.sub(clo, 2)
-			end
-		end
-
-		return clo
-	end
-
-	autopairs.remove_rule("(")
-	autopairs.remove_rule("{")
-	autopairs.remove_rule("[")
-
-	autopairs.add_rule(Rule("[%(%{%[]", "")
-		:use_regex(true)
-		:replace_endpair(function(opts)
-			return get_closing_for_line(opts.line)
-		end)
-		:end_wise(function(opts)
-			-- Do not endwise if there is no closing
-			return get_closing_for_line(opts.line) ~= ""
-		end))
 end
 
 return M
