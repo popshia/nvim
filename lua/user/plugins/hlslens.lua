bec2835ea5f1f654596e3c8ef29a84fbd03bb576
Noah Lin <noah@c-link.com.tw>
2024-01-11 10:00:36 +0800

    add mappings to center the cursor while searching.

--- lua/user/plugins/hlslens.lua
+++ lua/user/plugins/hlslens.lua
@@ -10,6 +10,17 @@ function M.config()
 		nearest_only = true,
 		nearest_float_when = "auto",
 	})
+
+	local map = function(mode, keys, func)
+		vim.keymap.set(mode, keys, func, { noremap = true, silent = true })
+	end
+
+	map("n", "n", [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR>zz<Cmd>lua require('hlslens').start()<CR>]])
+	map("n", "N", [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR>zz<Cmd>lua require('hlslens').start()<CR>]])
+	map("n", "*", [[*zz<Cmd>lua require('hlslens').start()<CR>]])
+	map("n", "#", [[#zz<Cmd>lua require('hlslens').start()<CR>]])
+	map("n", "g*", [[g*zz<Cmd>lua require('hlslens').start()<CR>]])
+	map("n", "g#", [[g#zz<Cmd>lua require('hlslens').start()<CR>]])
 end
 
 return M
