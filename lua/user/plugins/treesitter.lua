c3113b9f5fbf0684325c304c20383076f4b800a4
Noah Lin <noah@c-link.com.tw>
2024-01-29 14:34:36 +0800

    tweak timeoutlen and treesitter.

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -32,14 +32,18 @@ function M.config()
 			"python",
 			"vimdoc",
 		},
-		sync_install = true, -- install languages synchronously (only applied to `ensure_installed`)
+		sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
 		autopairs = {
 			enable = true,
 		},
 		highlight = {
 			enable = true, -- false will disable the whole extension
 			disable = { "" }, -- list of language that will be disabled
-			additional_vim_regex_highlighting = true,
+			additional_vim_regex_highlighting = false,
+			-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+			-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+			-- Using this option may slow down your editor, and you may see some duplicate highlights.
+			-- Instead of true it can also be a list of languages
 		},
 		indent = { enable = true, disable = { "css" } },
 	})
