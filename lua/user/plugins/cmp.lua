93f9b18e3db1a5688089e28069a8ee4dd775f090
Noah Lin <noah@c-link.com.tw>
2024-03-04 15:25:13 +0800

    update cmp to show buffer completions.

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -4,6 +4,7 @@ return {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
 		-- cmp sources
+		{ "hrsh7th/cmp-buffer" }, -- buffer completions
 		{ "hrsh7th/cmp-path" }, -- path completions
 		{ "hrsh7th/cmp-cmdline" }, -- cmdline completions
 		{ "hrsh7th/cmp-nvim-lsp" }, -- lsp completions
@@ -69,6 +70,9 @@ return {
 				{ name = "luasnip" },
 				{ name = "path" },
 			},
+			{
+				{ name = "buffer" },
+			},
 			confirm_opts = {
 				behavior = cmp.ConfirmBehavior.Replace,
 				select = false,
@@ -93,9 +97,17 @@ return {
 				},
 			},
 		})
+		-- Set configuration for specific filetype.
+		cmp.setup.filetype("gitcommit", {
+			sources = cmp.config.sources({
+				{ name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+			}, {
+				{ name = "buffer" },
+			}),
+		})
 
-		-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-		cmp.setup.cmdline("/", {
+		-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+		cmp.setup.cmdline({ "/", "?" }, {
 			mapping = cmp.mapping.preset.cmdline(),
 			sources = {
 				{ name = "buffer" },
@@ -109,14 +121,6 @@ return {
 				{ name = "path" },
 			}, {
 				{ name = "cmdline" },
-			}),
-		})
-
-		cmp.setup.filetype("gitcommit", {
-			sources = cmp.config.sources({
-				{ name = "git" }, -- You can specify the `cmp_git` source if you were installed it.
-			}, {
-				{ name = "buffer" },
 			}),
 		})
 	end,
