7a56d1c0b3dacd30650f1c53e431c685164d7800
Noah Lin <noah@c-link.com.tw>
2024-03-13 11:22:10 +0800

    add flash back.

---
+++ lua/user/plugins/flash.lua
@@ -0,0 +1,41 @@
+-- quickly jump to text
+
+return {
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	keys = {
+		{
+			"s",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash",
+		},
+		{
+			"r",
+			mode = "o",
+			function()
+				require("flash").remote()
+			end,
+			desc = "Remote Flash",
+		},
+	},
+	config = function()
+		require("flash").setup({
+			search = {
+				multi_window = false,
+			},
+			jump = {
+				nohlsearch = true,
+				autojump = true,
+			},
+			modes = {
+				char = {
+					enabled = false,
+					multi_line = false,
+				},
+			},
+		})
+	end,
+}
