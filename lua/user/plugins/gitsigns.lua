cca6d64f7777d736f190deb45aa8033b45cf37c7
Noah Lin <noah@c-link.com.tw>
2024-05-31 14:27:52 +0800

    move some keybindings.

--- lua/user/plugins/gitsigns.lua
+++ lua/user/plugins/gitsigns.lua
@@ -3,6 +3,17 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	event = "BufEnter",
+	keys = {
+		{ "]g", "<cmd>Gitsigns next_hunk<CR>", desc = "Next Hunk" },
+		{ "[g", "<cmd>Gitsigns prev_hunk<CR>", desc = "Prev Hunk" },
+		{ "<leader>gb", "<cmd>Gitsigns blame_line<CR>", desc = "Blame" },
+		{ "<leader>gh", "<cmd>Gitsigns preview_hunk<CR>", desc = "Preview Hunk" },
+		{ "<leader>gr", "<cmd>Gitsigns reset_hunk<CR>", desc = "Reset Hunk" },
+		{ "<leader>gR", "<cmd>Gitsigns reset_buffer<CR>", desc = "Reset Buffer" },
+		{ "<leader>gs", "<cmd>Gitsigns stage_hunk<CR>", desc = "Stage Hunk" },
+		{ "<leader>gu", "<cmd>Gitsigns undo_stage_hunk<CR>", desc = "Undo Stage Hunk" },
+		{ "<leader>gd", "<cmd>Gitsigns diffthis HEAD<CR>", desc = "Diff" },
+	},
 	config = function()
 		require("gitsigns").setup({
 			signs = {
