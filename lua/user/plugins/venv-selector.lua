4e36e721038c713ece49d43c364f83a1c4262144
Noah Lin <noah@c-link.com.tw>
2024-06-13 18:01:17 +0800

    add venv-selector.

---
+++ lua/user/plugins/venv-selector.lua
@@ -0,0 +1,26 @@
+return {
+	"linux-cultist/venv-selector.nvim",
+	branch = "regexp",
+	dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+	opts = {
+		options = {
+			fd_binary_name = "fdfind",
+		},
+		settings = {
+			search = {
+				anaconda_base = {
+					command = "$FD '/python$' /home/noah/miniconda3/bin --full-path --color never -E /proc",
+					type = "anaconda",
+				},
+				anaconda_envs = {
+					command = "$FD '/bin/python$' /home/noah/miniconda3/envs/ --full-path --color never -E /proc",
+					type = "anaconda",
+				},
+			},
+		},
+	},
+	event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+	keys = {
+		{ "<leader>vs", "<cmd>VenvSelect<cr>" },
+	},
+}
