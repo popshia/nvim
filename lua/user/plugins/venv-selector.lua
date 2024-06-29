cc19acedcc271c4b18609f14fa78e62b1de8377e
Noah Lin <noah@c-link.com.tw>
2024-06-29 11:32:36 +0800

    optimized venv-selector for different os.

--- lua/user/plugins/venv-selector.lua
+++ lua/user/plugins/venv-selector.lua
@@ -8,21 +8,32 @@ return {
 	keys = {
 		{ "<leader>vs", "<cmd>VenvSelect<cr>" },
 	},
-	opts = {
-		options = {
-			fd_binary_name = "fdfind",
-		},
-		settings = {
-			search = {
-				conda_base = {
-					command = "$FD '/python$' /home/noah/miniconda3/bin --full-path --color never -E /proc",
-					type = "anaconda",
-				},
-				conda_envs = {
-					command = "$FD '/bin/python$' /home/noah/miniconda3/envs/ --full-path --color never -E /proc",
-					type = "anaconda",
+	config = function()
+		local system = vim.loop.os_uname().sysname
+		local base_path = ""
+		local env_path = ""
+
+		if system == "Darwin" then
+			base_path = "$FD '/python$' /opt/homebrew/Caskroom/miniconda/base/bin/ --full-path --color never -E /proc"
+			env_path = "$FD '/python$' /opt/homebrew/Caskroom/miniconda/base/envs/ --full-path --color never -E /proc"
+		elseif system == "Linux" then
+			base_path = "$FD '/python$' /home/noah/miniconda3/bin/ --full-path --color never -E /proc"
+			env_path = "$FD '/python$' /home/noah/miniconda3/envs/ --full-path --color never -E /proc"
+		end
+
+		require("venv-selector").setup({
+			settings = {
+				search = {
+					conda_base = {
+						command = base_path,
+						type = "anaconda",
+					},
+					conda_envs = {
+						command = env_path,
+						type = "anaconda",
+					},
 				},
 			},
-		},
-	},
+		})
+	end,
 }
