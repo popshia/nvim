d8fe0e1aa5b95aead05fba1ba07195b549d2091b
Noah Lin <noah@c-link.com.tw>
2024-07-23 17:28:04 +0800

    update colorscheme configs.

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -6,15 +6,14 @@ return {
 	priority = 1000,
 	init = function()
 		-- g:gruvbox_material_background: hard, medium, soft
+		vim.g.gruvbox_material_background = "medium"
 		-- g:gruvbox_material_foreground & gruvbox_material_statusline_style: material, mix, original
-		vim.cmd([[
-		let g:gruvbox_material_background = "medium"
-		let g:gruvbox_material_foreground = "mix"
-		let g:gruvbox_material_statusline_style = "mix"
-		let g:gruvbox_material_enable_italic = 1
-		let g:gruvbox_material_enable_bold = 1
-		let g:gruvbox_material_better_performance = 1
-      ]])
+		vim.g.gruvbox_material_foreground = "mix"
+		vim.g.gruvbox_material_statusline_style = "mix"
+		vim.g.gruvbox_material_enable_italic = 1
+		vim.g.gruvbox_material_enable_bold = 1
+		vim.g.gruvbox_material_better_performance = 1
+		vim.g.gruvbox_material_dim_inactive_windows = 1
 		vim.cmd.colorscheme("gruvbox-material")
 	end,
 }
