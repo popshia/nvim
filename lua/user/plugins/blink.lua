ec17f930b9d346f0e895a8840d81ce1abfb18f01
Noah Lin <noah@c-link.com.tw>
2024-12-17 15:25:17 +0800

    test: replace cmp with blink.

---
+++ lua/user/plugins/blink.lua
@@ -0,0 +1,48 @@
+return {
+	"saghen/blink.cmp",
+	lazy = false,
+	dependencies = {
+		{
+			"L3MON4D3/LuaSnip",
+			build = "make install_jsregexp",
+			dependencies = { "rafamadriz/friendly-snippets" },
+		},
+	},
+	version = "v0.*",
+	opts = {
+		keymap = { preset = "enter" },
+		appearance = {
+			use_nvim_cmp_as_default = true,
+			nerd_font_variant = "mono",
+		},
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+		signature = { enabled = true },
+		completion = {
+			menu = {
+				draw = {
+					columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 } },
+				},
+			},
+			documentation = {
+				auto_show = true,
+			},
+		},
+		snippets = {
+			expand = function(snippet)
+				require("luasnip").lsp_expand(snippet)
+			end,
+			active = function(filter)
+				if filter and filter.direction then
+					return require("luasnip").jumpable(filter.direction)
+				end
+				return require("luasnip").in_snippet()
+			end,
+			jump = function(direction)
+				require("luasnip").jump(direction)
+			end,
+		},
+	},
+	opts_extend = { "sources.default" },
+}
