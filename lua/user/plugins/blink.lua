af5a7b0537e36525c9d4addcac88b619029fdc3c
Noah lin <noah@c-link.com.tw>
2025-03-14 18:03:24 +0800

    refactor(comments): remove blink comments.

--- lua/user/plugins/blink.lua
+++ lua/user/plugins/blink.lua
@@ -49,7 +49,6 @@ return {
          providers = {
             cmdline = {
                min_keyword_length = function(ctx)
-                  -- when typing a command, only show when the keyword is 3 characters or longer
                   if ctx.mode == "cmdline" and string.find(ctx.line, " ") == nil then return 3 end
                   return 0
                end,
