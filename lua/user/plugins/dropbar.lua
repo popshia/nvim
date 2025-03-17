803c7bd06ed51fb316cabe1745097d1256300254
Noah lin <noah@c-link.com.tw>
2025-03-17 16:49:03 +0800

    fix(dropbar): remove path from dropbar.

--- lua/user/plugins/dropbar.lua
+++ lua/user/plugins/dropbar.lua
@@ -20,10 +20,14 @@ return {
          sources = function(buf, _)
             local sources = require("dropbar.sources")
             local utils = require("dropbar.utils")
-            if vim.bo[buf].ft == "markdown" then return {
-               sources.path,
-               sources.markdown,
-            } end
+            if vim.bo[buf].ft == "markdown" then
+               return {
+                  -- sources.path,
+                  utils.source.fallback({
+                     sources.markdown,
+                  }),
+               }
+            end
             if vim.bo[buf].buftype == "terminal" then return {
                sources.terminal,
             } end
