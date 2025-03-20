553d49e905ffe00a3ae4695a07a435debf465092
Noah lin <noah@c-link.com.tw>
2025-03-20 18:38:00 +0800

    feat(colorscheme): add original gruvbox colorscheme.

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -2,6 +2,9 @@
 
 return {
    "sainnhe/gruvbox-material",
+   dependencies = {
+      "ellisonleao/gruvbox.nvim",
+   },
    lazy = false,
    priority = 1000,
    init = function()
@@ -14,6 +17,30 @@ return {
       vim.g.gruvbox_material_enable_bold = 1
       vim.g.gruvbox_material_better_performance = 1
       vim.g.gruvbox_material_dim_inactive_windows = 1
+      require("gruvbox").setup({
+         terminal_colors = true, -- add neovim terminal colors
+         undercurl = true,
+         underline = true,
+         bold = true,
+         italic = {
+            strings = true,
+            emphasis = true,
+            comments = true,
+            operators = false,
+            folds = true,
+         },
+         strikethrough = true,
+         invert_selection = false,
+         invert_signs = false,
+         invert_tabline = false,
+         invert_intend_guides = false,
+         inverse = true, -- invert background for search, diffs, statuslines and errors
+         contrast = "soft", -- can be "hard", "soft" or empty string
+         palette_overrides = {},
+         overrides = {},
+         dim_inactive = false,
+         transparent_mode = false,
+      })
       vim.cmd.colorscheme("gruvbox-material")
    end,
 }
