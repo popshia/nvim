b998153ef97ec664ba034d35f600f4a0251a317d
Noah lin <noah@c-link.com.tw>
2025-10-02 13:55:12 +0800

    update lazy setup.

--- lua/lazy-bootstrap.lua
+++ lua/lazy-bootstrap.lua
@@ -1,14 +1,17 @@
--- install lazy.nvim
+-- bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-   vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable", -- latest stable release
-      lazypath,
-   })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+   if vim.v.shell_error ~= 0 then
+      vim.api.nvim_echo({
+         { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+         { out, "WarningMsg" },
+         { "\nPress any key to exit..." },
+      }, true, {})
+      vim.fn.getchar()
+      os.exit(1)
+   end
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -17,15 +20,24 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Install your plugins here
-require("lazy").setup({ { import = "plugins" } }, {
-   colorscheme = { "gruvbox-material" },
+-- setup lazy.nvim
+require("lazy").setup({
+   spec = {
+      { import = "plugins" },
+   },
+   install = {
+      colorscheme = { "gruvbox-material" },
+   },
+   checker = {
+      enabled = true,
+      notify = true,
+   },
    ui = {
       border = "rounded",
    },
    change_detection = {
       enabled = true,
-      notify = false,
+      notify = true,
    },
 })
 
