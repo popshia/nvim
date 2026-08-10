e488913547e4d2d4ad542b23cbdff18a1cdf9251
Noah Lin <noah@c-link.com.tw>
2026-08-10 11:21:46 +0800

    update xcodebuild plugin match pack specs and pack-ui.

--- bench/swift/xcodebuild.lua
+++ bench/swift/xcodebuild.lua
@@ -1,38 +1,29 @@
 -- xcodebuild
 
-return {
-   "wojciech-kulik/xcodebuild.nvim",
-   ft = { "swift" },
-   dependencies = {
-      "nvim-telescope/telescope.nvim",
-      "MunifTanjim/nui.nvim",
-   },
-   keys = {
-      { "<leader>X", "<cmd>XcodebuildPicker<cr>", desc = "Show All Xcoddbuild Actions" },
-      { "<leader>xb", "<cmd>XcodebuildBuild<cr>", desc = "Build Project" },
-      { "<leader>xB", "<cmd>XcodebuildBuildForTesting<cr>", desc = "Build Project For Testing" },
-      { "<leader>xr", "<cmd>XcodebuildBuildRun<cr>", desc = "Build & Run Project" },
-      { "<leader>xt", "<cmd>XcodebuildTest<cr>", mode = { "n" }, desc = "Run Tests" },
-      { "<leader>xt", "<cmd>XcodebuildTestSelected<cr>", mode = { "v" }, desc = "Run Selected Tests" },
-      { "<leader>xp", "<cmd>XcodebuildSelectTestPlan<cr>", desc = "Select Test Plan" },
-      { "<leader>xT", "<cmd>XcodebuildTestClass<cr>", desc = "Run Current Test Class" },
-      { "<leader>x.", "<cmd>XcodebuildRepeat<cr>", desc = "Repeat Last Test Run" },
-      { "<leader>xl", "<cmd>XcodebuildToggleLogs<cr>", desc = "Toggle Xcodebuild Logs" },
-      { "<leader>xc", "<cmd>XcodebuildToggleCodeCoverage<cr>", desc = "Toggle Code Coverage" },
-      { "<leader>xC", "<cmd>XcodebuildShowCodeCoverageReport<cr>", desc = "Show Code Coverage Report" },
-      { "<leader>xe", "<cmd>XcodebuildTestExplorerToggle<cr>", desc = "Toggle Test Explorer" },
-      { "<leader>xs", "<cmd>XcodebuildFailingSnapshots<cr>", desc = "Show Failing Snapshots" },
-      { "<leader>xp", "<cmd>XcodebuildPreviewGenerateAndShow<cr>", desc = "Generate Preview" },
-      { "<leader>x<cr>", "<cmd>XcodebuildPreviewToggle<cr>", desc = "Toggle Preview" },
-      { "<leader>xd", "<cmd>XcodebuildSelectDevice<cr>", desc = "Select Device" },
-      { "<leader>xx", "<cmd>XcodebuildQuickfixLine<cr>", desc = "Quickfix Line" },
-      { "<leader>xa", "<cmd>XcodebuildCodeActions<cr>", desc = "Show Code Actions" },
-   },
-   config = function()
-      require("xcodebuild").setup({
-         code_coverage = {
-            enabled = true,
-         },
-      })
-   end,
-}
+on_filetype("swift", function()
+   vim.pack.add({ gh("wojciech-kulik/xcodebuild.nvim"), gh("MunifTanjim/nui.nvim") })
+   require("xcodebuild").setup({
+      code_coverage = {
+         enabled = true,
+      },
+   })
+   map("n", "<leader>X", "<cmd>XcodebuildPicker<cr>", "Show all Xcodebuild actions")
+   map("n", "<leader>xb", "<cmd>XcodebuildBuild<cr>", "Build Project")
+   map("n", "<leader>xB", "<cmd>XcodebuildBuildForTesting<cr>", "Build Project For Testing")
+   map("n", "<leader>xr", "<cmd>XcodebuildBuildRun<cr>", "Build & Run Project")
+   map("n", "<leader>xt", "<cmd>XcodebuildTest<cr>", "Run Tests")
+   map("n", "<leader>xt", "<cmd>XcodebuildTestSelected<cr>", "Run Selected Tests")
+   map("n", "<leader>xp", "<cmd>XcodebuildSelectTestPlan<cr>", "Select Test Plan")
+   map("n", "<leader>xT", "<cmd>XcodebuildTestClass<cr>", "Run Current Test Class")
+   map("n", "<leader>x.", "<cmd>XcodebuildRepeat<cr>", "Repeat Last Test Run")
+   map("n", "<leader>xl", "<cmd>XcodebuildToggleLogs<cr>", "Toggle Xcodebuild Logs")
+   map("n", "<leader>xc", "<cmd>XcodebuildToggleCodeCoverage<cr>", "Toggle Code Coverage")
+   map("n", "<leader>xC", "<cmd>XcodebuildShowCodeCoverageReport<cr>", "Show Code Coverage Report")
+   map("n", "<leader>xe", "<cmd>XcodebuildTestExplorerToggle<cr>", "Toggle Test Explorer")
+   map("n", "<leader>xs", "<cmd>XcodebuildFailingSnapshots<cr>", "Show Failing Snapshots")
+   map("n", "<leader>xp", "<cmd>XcodebuildPreviewGenerateAndShow<cr>", "Generate Preview")
+   map("n", "<leader>x<cr>", "<cmd>XcodebuildPreviewToggle<cr>", "Toggle Preview")
+   map("n", "<leader>xd", "<cmd>XcodebuildSelectDevice<cr>", "Select Device")
+   map("n", "<leader>xx", "<cmd>XcodebuildQuickfixLine<cr>", "Quickfix Line")
+   map("n", "<leader>xa", "<cmd>XcodebuildCodeActions<cr>", "Show Code Actions")
+end)
